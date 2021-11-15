d18fb316f7d661cdb6a5c7aeaffd0ed4fd70a00d
Aser <71518609+ASER-VANT@users.noreply.github.com>
2021-11-15 12:48:39 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,89 +1,43 @@
 #!/bin/bash
-
 # Author : SAEP ASER
 
-
-
-
-
 kirmizi="\e[31m"
-
 yesil="\e[32m"
-
 sari="\e[33m"
-
 mor="\e[34m"
-
 mavi="\e[36m"
-
 beyaz="\033[1;37m"
-
 reset="\e[0m"
 
-
-
 apt install figlet
-
 clear
-
 figlet HUNTER
-
 printf "\e[1;77m\e[41m     KURULUMLARINIZ 3 SANİYE İÇİNDE BAŞLAYACAKTIR!!     \e[0m\n"
-
 sleep 3
-
 termux-setup-storage -y
-
-pkg install root-repo -y
-
-pkg install unstable-repo
-
-pkg install x11-repo -y
-
-apt update -y
-
+pkg install root-repo unstable-repo x11-repo -y
+apt install game-repo
+apt purge game-repo
+apt update
 apt upgrade -y
-
-pkg install git -y
-
-pkg install python python2 -y
-
-pkg intall python3 -y
-
-pkg install pip -y
-
-apt install purge-repo -y
-
-pkg install php -y
-
-pkg install perl -y
-
-pkg install nano -y
-
-pkg install cat -y
-
-pkg install pip2 -y
-
+apt install git -y
+apt install python python2 -y
+apt intall python3 -y
+apt install pip -y
+apt install php -y
+apt install perl -y
+apt install nano -y
+apt install cat -y
+apt install pip2 -y
 pip install wordlist -y
-
-pkg install nmap -y
-
-pkg install hydra -y
-
-pkg install openssl -y
-
+apt install nmap -y
+apt install hydra -y
+apt install openssl -y
 apt install nodejs -y
-
-pkg install cmatrix
-
-alias eğitim='python3 Termux-Eğitim.py'
-
+apt install cmatrix
+alias egitim='python3 Termux-Eğitim.py'
 clear
 
-
-
-printf "\e[1;77m\e[41m                 KURULUM BİTTİ!!                   \e[0m\n"
-
+printf "$kirmizi                 KURULUM BİTTİ!!                   $reset"
 printf "\n"
-
-printf "\e[1;77m\e[41m     eğitim  YAZARAK ARACI ÇALIŞTIRABİLİRSİNİZ     \e[0m\n"
+printf "$kirmizi     egitim  YAZARAK ARACI ÇALIŞTIRABİLİRSİNİZ     $reset"
